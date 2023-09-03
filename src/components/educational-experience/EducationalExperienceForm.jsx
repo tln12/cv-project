934b43593b8326863c1494e4966638067b069587
Nho Mac <thanh.le.nguyen@rwth-aachen.de>
2023-09-03 17:58:55 +0200

    Initial commit

--- src/components/educational-experience/EducationalExperienceForm.jsx
+++ src/components/educational-experience/EducationalExperienceForm.jsx
@@ -1,6 +1,7 @@
 import EducationalExperienceFormEntry from "./EducationalExperienceFormEntry";
 
 export default function EducationalExperienceForm({ education, handleChange, handleSubmit, handleEdit}) {
+    
     return(
         <div id="ee">
             <h2>Educational Experience</h2>
