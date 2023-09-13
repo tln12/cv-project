5fa8ab0b8fe5865ff15f1a7fa15e50b95e97cbbb
Nho Mac <thanh.le.nguyen@rwth-aachen.de>
2023-09-13 15:17:09 +0200

    wrap collapsible with div.collapsible

--- src/components/collapsible/Collapsible.jsx
+++ src/components/collapsible/Collapsible.jsx
@@ -10,12 +10,12 @@ export default function Collapsible({ title, content, collapsed = true }) {
     }
 
     return(
-        <>
-            <h2 className='collapsible-title collapsible'>
+        <div className="collapsible">
+            <h2 className='title'>
                 <span>{title}</span>
                 <button className="material-symbols-outlined" onClick={handleCollapse}>{open ? 'arrow_drop_down': 'arrow_right'}</button>
             </h2>
             <div className={'content ' + (open ? '': 'collapsed')}>{content}</div>
-        </>
+        </div>
     );
 }
