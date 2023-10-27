f637a3b83ed9eaf65b575e7ecfb003ac36906a57
Nho Mac <thanh.le.nguyen@rwth-aachen.de>
2023-10-27 22:39:41 +0200

    migrate style of collapsible to its component

--- src/components/collapsible/Collapsible.jsx
+++ src/components/collapsible/Collapsible.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react';
-import './Collapsible.css';
+import styled from 'styled-components';
+
 /** Collapsible Button */
+
+const StyledHeader = styled.h2`
+  padding: 15px var(--padding-lr-control);
+  display: flex;
+  justify-content: space-between;
+`;
+const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  display: ${(props) => !props.open && 'none'};
+`;
 
 export default function Collapsible({ title, content, collapsed = true }) {
   const [open, setOpen] = useState(!collapsed);
@@ -10,14 +22,14 @@ export default function Collapsible({ title, content, collapsed = true }) {
   }
 
   return (
-    <div className='collapsible'>
-      <h2 className='title'>
+    <div>
+      <StyledHeader>
         <span>{title}</span>
         <button className='material-symbols-outlined' onClick={handleCollapse}>
           {open ? 'arrow_drop_down' : 'arrow_right'}
         </button>
-      </h2>
-      <div className={'content ' + (open ? '' : 'collapsed')}>{content}</div>
+      </StyledHeader>
+      <StyledContent open={open}>{content}</StyledContent>
     </div>
   );
 }
