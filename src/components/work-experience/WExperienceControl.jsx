1ce374c4377921d9897a5364722cabcd0fbd6752
Nho Mac <thanh.le.nguyen@rwth-aachen.de>
2023-10-27 21:41:09 +0200

    migrate WEForm into separate component

--- src/components/work-experience/WExperienceControl.jsx
+++ src/components/work-experience/WExperienceControl.jsx
@@ -1,76 +1,6 @@
 import Collapsible from '../collapsible/Collapsible';
 import Register from '../Register';
-import './WExperience.css';
-
-function WEForm({ formData, handleChange, handleReturn, mode, handleSubmit }) {
-  return (
-    <form className='we-c-form' data-id={formData.id} onSubmit={handleSubmit}>
-      {mode == 'edit' && (
-        <button
-          className='return-btn material-symbols-outlined'
-          onClick={handleReturn}
-        >
-          arrow_back
-        </button>
-      )}
-      <div className='we-date'>
-        <label htmlFor='starting-date'>
-          <span>starting date</span>
-          <input
-            type='date'
-            id='starting date'
-            value={formData.startingDate}
-            onChange={handleChange}
-          ></input>
-        </label>
-        <label htmlFor='end-date'>
-          <span>end date</span>
-          <input
-            type='date'
-            id='end-date'
-            value={formData.endDate}
-            onChange={handleChange}
-          ></input>
-        </label>
-      </div>
-      <label htmlFor='name'>
-        <span>company name</span>
-        <input id='name' value={formData.name} onChange={handleChange}></input>
-      </label>
-      <label htmlFor='title'>
-        <span>position title</span>
-        <input
-          id='title'
-          value={formData.title}
-          onChange={handleChange}
-        ></input>
-      </label>
-      <label htmlFor='description'>
-        <span>description</span>
-        <textarea
-          id='description'
-          rows='5'
-          value={formData.description}
-          onChange={handleChange}
-        ></textarea>
-      </label>
-      <div className='we-c-submit'>
-        {mode == 'create' && (
-          <button
-            className='material-symbols-outlined'
-            type='button'
-            onClick={handleReturn}
-          >
-            close
-          </button>
-        )}
-        <button className='material-symbols-outlined' type='submit'>
-          check
-        </button>
-      </div>
-    </form>
-  );
-}
+import WExperienceForm from './WExperienceForm';
 
 export default function WExperienceControl(props) {
   const {
@@ -99,7 +29,7 @@ export default function WExperienceControl(props) {
     );
   } else if (controlStatus.render == 'form') {
     content = (
-      <WEForm
+      <WExperienceForm
         formData={formData}
         handleChange={handleChange}
         handleReturn={handleReturn}
---
+++ src/components/work-experience/WExperienceForm.jsx
@@ -0,0 +1,88 @@
+import {
+  StyledForm,
+  StyledLabel,
+  StyledSpan,
+  StyledInputGroup,
+  StyledReturnButton,
+  StyledSubmitPanel,
+  StyledSubmitPanelButton,
+} from '../FormComponents';
+
+export default function WExperienceForm({
+  formData,
+  handleChange,
+  handleReturn,
+  mode,
+  handleSubmit,
+}) {
+  return (
+    <StyledForm data-id={formData.id} onSubmit={handleSubmit}>
+      {mode == 'edit' && (
+        <StyledReturnButton
+          className='return-btn material-symbols-outlined'
+          onClick={handleReturn}
+        >
+          arrow_back
+        </StyledReturnButton>
+      )}
+      <StyledInputGroup>
+        <StyledLabel htmlFor='starting-date'>
+          <StyledSpan>starting date</StyledSpan>
+          <input
+            type='date'
+            id='starting date'
+            value={formData.startingDate}
+            onChange={handleChange}
+          ></input>
+        </StyledLabel>
+        <StyledLabel htmlFor='end-date'>
+          <StyledSpan>end date</StyledSpan>
+          <input
+            type='date'
+            id='end-date'
+            value={formData.endDate}
+            onChange={handleChange}
+          ></input>
+        </StyledLabel>
+      </StyledInputGroup>
+      <StyledLabel htmlFor='name'>
+        <StyledSpan>company name</StyledSpan>
+        <input id='name' value={formData.name} onChange={handleChange}></input>
+      </StyledLabel>
+      <StyledLabel htmlFor='title'>
+        <StyledSpan>position title</StyledSpan>
+        <input
+          id='title'
+          value={formData.title}
+          onChange={handleChange}
+        ></input>
+      </StyledLabel>
+      <StyledLabel htmlFor='description'>
+        <StyledSpan>description</StyledSpan>
+        <textarea
+          id='description'
+          rows='5'
+          value={formData.description}
+          onChange={handleChange}
+        ></textarea>
+      </StyledLabel>
+      <StyledSubmitPanel>
+        {mode == 'create' && (
+          <StyledSubmitPanelButton
+            className='material-symbols-outlined'
+            type='button'
+            onClick={handleReturn}
+          >
+            close
+          </StyledSubmitPanelButton>
+        )}
+        <StyledSubmitPanelButton
+          className='material-symbols-outlined'
+          type='submit'
+        >
+          check
+        </StyledSubmitPanelButton>
+      </StyledSubmitPanel>
+    </StyledForm>
+  );
+}
