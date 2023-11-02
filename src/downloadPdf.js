341b2efc6d70d1e88ff6d0ffc4f8871e412118e7
Nho Mac <thanh.le.nguyen@rwth-aachen.de>
2023-11-02 19:04:29 +0100

    add todays date to downloaded pdf

--- src/downloadPdf.js
+++ src/downloadPdf.js
@@ -2,12 +2,13 @@ import jsPDF from 'jspdf';
 
 export default function downloadHTMLAsPDF() {
   let target = document.getElementById('cv');
+  const date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
   const doc = new jsPDF();
   doc.setFont('helvetica');
   doc.html(target, {
     callback: function (doc) {
       doc.deletePage(2);
-      doc.save('sample-document.pdf');
+      doc.save(`CV_${date}`);
     },
     width: 210, //target width in the PDF document
     windowWidth: 750, //window width in CSS pixels
