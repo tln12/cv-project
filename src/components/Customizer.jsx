47fa83f8647557cd130a27b97020752e4ffbf9b4
Nho Mac <thanh.le.nguyen@rwth-aachen.de>
2023-11-04 22:49:39 +0100

    summarize styles; change style of heading

--- src/components/Customizer.jsx
+++ src/components/Customizer.jsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 
-const StyledSection = styled.div`
+const StyledBox = styled.div`
   background-color: white;
   border-radius: var(--bar-control);
   border: solid var(--side-font-color);
-`;
-const StyledContent = styled.div`
   padding: var(--padding-tb-form-control) var(--padding-lr-control);
 `;
 const Styledbutton = styled.button`
@@ -21,59 +19,52 @@ const StyledList = styled.ul`
   display: flex;
   gap: 10px;
 `;
-const StyledHeadingTwo = styled.h2`
-  padding: var(--padding-tb-form-control);
-`;
 
 function ColorPick({ cvColor, setCVColor }) {
   return (
-    <StyledSection>
-      <StyledHeadingTwo>Theme Color</StyledHeadingTwo>
-      <StyledContent>
-        <StyledColorInput
-          value={cvColor}
-          onChange={(e) => setCVColor(e.target.value)}
-        ></StyledColorInput>
-      </StyledContent>
-    </StyledSection>
+    <StyledBox>
+      <h2>Theme Color</h2>
+      <StyledColorInput
+        value={cvColor}
+        onChange={(e) => setCVColor(e.target.value)}
+      ></StyledColorInput>
+    </StyledBox>
   );
 }
 function FontPick({ cvFont, setCVFont }) {
   return (
-    <StyledSection>
-      <StyledHeadingTwo>Font</StyledHeadingTwo>
-      <StyledContent>
-        <StyledList>
-          <li>
-            <Styledbutton
-              style={{ fontFamily: 'helvetica' }}
-              onClick={() => setCVFont('helvetica')}
-              $primary={cvFont === 'helvetica'}
-            >
-              Helvetica
-            </Styledbutton>
-          </li>
-          <li>
-            <Styledbutton
-              style={{ fontFamily: 'courier' }}
-              onClick={() => setCVFont('courier')}
-              $primary={cvFont === 'courier'}
-            >
-              Courier
-            </Styledbutton>
-          </li>
-          <li>
-            <Styledbutton
-              style={{ fontFamily: 'times' }}
-              onClick={() => setCVFont('times')}
-              $primary={cvFont === 'times'}
-            >
-              Times New Roman
-            </Styledbutton>
-          </li>
-        </StyledList>
-      </StyledContent>
-    </StyledSection>
+    <StyledBox>
+      <h2>Font</h2>
+      <StyledList>
+        <li>
+          <Styledbutton
+            style={{ fontFamily: 'helvetica' }}
+            onClick={() => setCVFont('helvetica')}
+            $primary={cvFont === 'helvetica'}
+          >
+            Helvetica
+          </Styledbutton>
+        </li>
+        <li>
+          <Styledbutton
+            style={{ fontFamily: 'courier' }}
+            onClick={() => setCVFont('courier')}
+            $primary={cvFont === 'courier'}
+          >
+            Courier
+          </Styledbutton>
+        </li>
+        <li>
+          <Styledbutton
+            style={{ fontFamily: 'times' }}
+            onClick={() => setCVFont('times')}
+            $primary={cvFont === 'times'}
+          >
+            Times New Roman
+          </Styledbutton>
+        </li>
+      </StyledList>
+    </StyledBox>
   );
 }
 
