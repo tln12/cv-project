29e17ebcd66fc2f8c3e1836b3747e89c2cd654be
Nho Mac <thanh.le.nguyen@rwth-aachen.de>
2023-11-05 00:35:54 +0100

    fix overflow of app

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import * as exampleData from './exampleData.js';
 import styled from 'styled-components';
 
 const StyledApp = styled.div`
-  flex: 1;
   display: flex;
-  width: 100vw;
+  width: 100%;
+  height: 100%;
 `;
 
 function App() {
