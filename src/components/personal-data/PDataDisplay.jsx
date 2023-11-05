e2a1e703a9bfb5d3150b9d4a4c4e2f72e60dbfbc
Nho Mac <thanh.le.nguyen@rwth-aachen.de>
2023-11-05 17:29:44 +0100

    remove display of name

--- src/components/personal-data/PDataDisplay.jsx
+++ src/components/personal-data/PDataDisplay.jsx
@@ -1,11 +1,5 @@
 import styled from 'styled-components';
 
-const StyledHeader = styled.h2`
-  letter-spacing: 1px;
-  span:first-of-type {
-    font-weight: bold;
-  }
-`;
 const StyledEntry = styled.li`
   display: flex;
 `;
@@ -14,34 +8,27 @@ const StyledSpan = styled.span`
 `;
 
 export default function PDataDisplay({ personalData, StyledLine }) {
-  const { firstName, lastName, street, houseNr, postcode, city, phone, email } =
-    personalData;
+  const { street, houseNr, postcode, city, phone, email } = personalData;
   return (
-    <>
-      <StyledHeader>
-        <span>{firstName}&nbsp;</span>
-        <span>{lastName}</span>
-      </StyledHeader>
-      <section>
-        <h3>PERSONAL DETAILS</h3>
-        <StyledLine />
-        <ul>
-          <StyledEntry>
-            <StyledSpan>Address</StyledSpan>
-            <StyledSpan>
-              {street} {houseNr}, {postcode} {city}
-            </StyledSpan>
-          </StyledEntry>
-          <StyledEntry>
-            <StyledSpan>Tel.</StyledSpan>
-            <StyledSpan>{phone}</StyledSpan>
-          </StyledEntry>
-          <StyledEntry>
-            <StyledSpan>Email</StyledSpan>
-            <StyledSpan>{email}</StyledSpan>
-          </StyledEntry>
-        </ul>
-      </section>
-    </>
+    <section>
+      <h3>PERSONAL DETAILS</h3>
+      <StyledLine />
+      <ul>
+        <StyledEntry>
+          <StyledSpan>Address</StyledSpan>
+          <StyledSpan>
+            {street} {houseNr}, {postcode} {city}
+          </StyledSpan>
+        </StyledEntry>
+        <StyledEntry>
+          <StyledSpan>Tel.</StyledSpan>
+          <StyledSpan>{phone}</StyledSpan>
+        </StyledEntry>
+        <StyledEntry>
+          <StyledSpan>Email</StyledSpan>
+          <StyledSpan>{email}</StyledSpan>
+        </StyledEntry>
+      </ul>
+    </section>
   );
 }
