ac20ff515e653a43c5f8dcfbf9f6575b44781eef
Nho Mac <thanh.le.nguyen@rwth-aachen.de>
2023-11-12 21:25:40 +0100

    style button

--- src/components/DownloadPanel.jsx
+++ src/components/DownloadPanel.jsx
@@ -1,12 +1,18 @@
 import downloadPDF from '../downloadPdf';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: var(--shadow-color);
+  border: none;
+`;
 
 export default function DownloadPanel({ cvFont }) {
   return (
     <>
-      <button onClick={() => downloadPDF({ cvFont })}>
+      <StyledButton onClick={() => downloadPDF({ cvFont })}>
         <span className='material-symbols-outlined'>download</span>Download as
         PDF
-      </button>
+      </StyledButton>
     </>
   );
 }
